84dd6c8ac8632ff22a711ea2d756476d4093838c
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-10-24 17:13:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -109,7 +109,7 @@ if st.button("Run YOLOv5 Detection"):
                "--iou-thres", iou]
     process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     st.write(process)
-    time.sleep(7)
+    # time.sleep(7)
 
 # if st.button("Run YOLOv5 Detection"):
     for root, dirs, files in os.walk(get_detection_folder()):
