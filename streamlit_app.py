751724da8de53d03ab249c5ef145c3ee31e49e5c
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-10-24 14:29:30 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -98,6 +98,7 @@ def detect_diameter(namefile_txt, num_values):
 st.title('YOLOv5 Streamlit App')
 if st.button("Run YOLOv5 Detection"):
     int_image_path = f'images/{uploaded_file.name}'
+    st.write(int_image_path)
     path_detect_py = 'yolov5/detect.py'
     iou = '0.1'
     path_weight = "yolov5/runs/train/exp/weights/best.pt"
