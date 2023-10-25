fd3f01988be2e4b43724f32b4971884e842ede57
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-10-25 16:06:23 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,14 +7,13 @@ import os
 import shutil
 import subprocess
 import time
-import torch 
 # -------------------------Input------------------------------#
 # make a new folder save image
 folder = os.path.join('images')
 if not os.path.exists(folder):
     os.makedirs(folder)
 
-folder_detect = os.path.join('yolov5/runs/detect')
+folder_detect = os.path.join('detect')
 if not os.path.exists(folder_detect):
     os.makedirs(folder_detect)
 
@@ -47,8 +46,8 @@ def get_subdirs(b='.'):
     return result
 #  return newest folder
 def get_detection_folder():
-    return max(get_subdirs(os.path.join('yolov5/runs/detect')), key=os.path.getmtime)
-
+    # return max(get_subdirs(os.path.join('yolov5/runs/detect')), key=os.path.getmtime)
+    return max(get_subdirs(os.path.join('detect')), key=os.path.getmtime)
  # caculation diameter
 diameter_core = []
 diameter_shell = []
@@ -99,72 +98,66 @@ def detect_diameter(namefile_txt, num_values):
 st.title('YOLOv5 Streamlit App')
 if st.button("Run YOLOv5 Detection"):
     int_image_path = f'images/{uploaded_file.name}'
-    # path_detect_py = 'yolov5/detect.py'
-    path_detect_py = 'detect.py'
+    path_detect_py = 'yolov5/detect.py'
     iou = '0.1'
-    out_path = 'yolov5/runs/detect'
     path_weight = "yolov5/runs/train/exp/weights/best.pt"
+    uot_path = 'detect'
     command = ["python", path_detect_py,
                "--source", int_image_path,
                "--save-txt",
                "--weights", path_weight,
                "--iou-thres", iou,
-               "--project", out_path]
-    process = subprocess.Popen(command,shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-   
-    stdout, stderr = process.communicate()
-    if process.returncode == 0:
-        st.write("Quá trình chạy thành công.")
-        st.write("Standard Output:")
-        st.write(stdout.decode("utf-8"))  # Đọc đầu ra chuẩn và giải mã nó từ bytes sang chuỗi
-    else:
-        st.write("Quá trình chạy không thành công.")
-        st.write("Standard Error:")
-        st.write(stderr.decode("utf-8"))
-        
+               '--project', uot_path]
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    # time.sleep(7)
     process.wait()
 
     for root, dirs, files in os.walk(get_detection_folder()):
+        path_detect = root
         for file in files:
             if file.endswith('.txt'):
                 namefile_txt = os.path.join(root, file)
                 detect_diameter(namefile_txt, number)
                 st.write("Diameter core is:", D_core)
-                st.write("Diameter core is:", D_shell)
-                st.write(namefile_txt)
+
             if (file.endswith('.png') or file.endswith('.jpg') or file.endswith('.jpeg')):
                 namefile_img = os.path.join(root, file)
                 st.image(namefile_img, caption='Image detected')
-                
-    col1, col2 = st.columns([3, 1])
-    with col1:
-        # Tạo biểu đồ histogram core
-        plt.figure(dpi=300)
-        plt.hist(diameter_core, bins=10, color='b', alpha=0.7)
-        plt.ylabel('Frequency')
-        plt.xlabel('Partical Diameter')
-        plt.title('Histogram core')
-        st.pyplot(plt)
-        # Tạo biểu đồ histogram shell
-        plt.figure(dpi=300)
-        plt.hist(diameter_shell, bins=10, color='g', alpha=0.7)
-        plt.ylabel('Frequency')
-        plt.xlabel('Partical Diameter')
-        plt.title('Histogram shell')
-        st.pyplot(plt)
+    if np.isnan(D_core):
+        st.write(
+            "Please reformat the scale bar on the image by using the paint app or Paint 3D to redraw the scale bar so that the length remains the same and the width increases 2-3 times.")
+    else:
+        col1, col2 = st.columns([3, 1])
+        with col1:
+            # Tạo biểu đồ histogram core
+            plt.figure(dpi=300)
+            plt.hist(diameter_core, bins=10, color='b', alpha=0.7)
+            plt.ylabel('Frequency')
+            plt.xlabel('Partical Diameter')
+            plt.title('Histogram core')
+            st.pyplot(plt)
+            # Tạo biểu đồ histogram shell
+            st.write("Diameter shell is:", D_shell)
+            plt.figure(dpi=300)
+            plt.hist(diameter_shell, bins=10, color='g', alpha=0.7)
+            plt.ylabel('Frequency')
+            plt.xlabel('Partical Diameter')
+            plt.title('Histogram shell')
+            st.pyplot(plt)
 
-    with col2:
-        st.subheader("Diameter core")
-        data = pd.DataFrame(({"Diameter_core": diameter_core[:]}))
-        st.dataframe(data, height=300, width=200)
+        with col2:
+            st.subheader("Diameter core")
+            data = pd.DataFrame(({"Diameter_core": diameter_core[:]}))
+            st.dataframe(data, height=300, width=200)
 
-        st.subheader("Diameter shell")
-        data = pd.DataFrame(({'Diameter_shell': diameter_shell[:]}))
-        st.dataframe(data, height=300, width=200)
+            st.subheader("Diameter shell")
+            data = pd.DataFrame(({'Diameter_shell': diameter_shell[:]}))
+            st.dataframe(data, height=300, width=200)
 
     # Xóa tệp hình ảnh tạm thời
+    process.wait()
     os.remove(int_image_path)
-    time.sleep(7)
-    # shutil.rmtree('yolov5/runs/detect')
+    # time.sleep(7)
+    shutil.rmtree('detect')
 
 
