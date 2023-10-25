52748127da2be586bf84c14acaa2fca944e94e76
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-10-25 16:29:22 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ if st.button("Run YOLOv5 Detection"):
                "--weights", path_weight,
                "--iou-thres", iou,
                '--project', uot_path]
-    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    process = subprocess.Popen(command, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     # time.sleep(7)
     # process.wait()
     stdout, stderr = process.communicate()
