1117b788afbdea89526fcb3ed91ab6567c7b9df0
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-10-26 22:25:18 +0700

    Update detect.py

--- yolov5/detect.py
+++ yolov5/detect.py
@@ -1,4 +1,3 @@
-import torch
 import argparse
 import csv
 import os
@@ -19,7 +18,7 @@ from utils.dataloaders import IMG_FORMATS, VID_FORMATS, LoadImages, LoadScreensh
 from utils.general import (LOGGER, Profile, check_file, check_img_size, check_imshow, check_requirements, colorstr, cv2,
                            increment_path, non_max_suppression, print_args, scale_boxes, strip_optimizer, xyxy2xywh)
 from utils.torch_utils import select_device, smart_inference_mode
-
+import torch
 
 @smart_inference_mode()
 def run(
