96e60e051acc82ce408d30948dd0782fe654cf93
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-10-31 08:18:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,7 +7,7 @@ import os
 import shutil
 import subprocess
 import torch 
-import ultralitics
+import ultralytics
 
 # -------------------------Input------------------------------#
 # make a new folder save image
