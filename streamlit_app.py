90d5243788f37aa4369013a157e00186dac5c51c
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-01 11:16:10 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -160,4 +160,4 @@ if st.button("Run YOLOv5 Detection"):
     process.wait()
     os.remove(int_image_path)
     # time.sleep(7)
-    shutil.rmtree('yolov5/runs/detect')
+    # shutil.rmtree('yolov5/runs/detect')
