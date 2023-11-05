d698c6c11de068716a234f17f2ca9f76505057ea
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-05 21:55:08 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -23,12 +23,9 @@ from utils.torch_utils import select_device, smart_inference_mode
 
 
 def detect(source):
-    weights=ROOT / 'yolov5s.pt',  # model path or triton URL
-    source=ROOT / 'data/images',  # file/dir/URL/glob/screen/0(webcam)
-    data=ROOT / 'data/coco128.yaml',  # dataset.yaml path
     imgsz=(640, 640),  # inference size (height, width)
-    conf_thres=0.25,  # confidence threshold
-    iou_thres=0.45,  # NMS IOU threshold
+    conf_thres=0.55,  # confidence threshold
+    iou_thres=0.1,  # NMS IOU threshold
     max_det=1000,  # maximum detections per image
     device='',  # cuda device, i.e. 0 or 0,1,2,3 or cpu
     view_img=False,  # show results
