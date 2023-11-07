d9d7c1b3538224db97371d0372cb99dca2c9c569
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-07 22:42:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -92,12 +92,14 @@ def detect_diameter(namefile_txt, num_values):
     diameter_core = np.array(diameter_core)
     diameter_shell = np.array(diameter_shell)
 
+    mean_shell = mean(diameter_shell)
     if (len(diameter_core) > 0):
         for i in range(0, len(diameter_core)):
             if len(diameter_shell)==0:
                 diameter_core1 = diameter_core
                 break
-            if diameter_core[i] < min(diameter_shell):
+            # if diameter_core[i] < min(diameter_shell):
+            if diameter_core[i] < mean_shell):
                 diameter_core1 = diameter_core1 + [diameter_core[i]]
         D_core = np.mean(diameter_core1)
     else:
