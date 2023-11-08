01551c5c405c8f0866d7fd06e015f530e46aa3da
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-08 13:23:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -120,7 +120,7 @@ def detect_diameter(namefile_txt, num_values):
         D_shell = np.NaN  
 
     if not np.isnan(D_core) and not np.isnan(D_shell) :
-        if ((len(diameter_core1)/len(diameter_shell1))<(1/3) ) and ((D_shell - D_core) < 0.05*num_values):
+        if ((len(diameter_core1)/len(diameter_shell1))<(1/3) ) or ((D_shell - D_core) < 0.05*num_values):
             D_core = np.NaN
         
  # ------------------------------# run detect.py in yolov5----------------------------------------
