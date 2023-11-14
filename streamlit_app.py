02a8dd121bc2aed9a240ecee28b585a4fbce9530
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-15 03:20:39 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -132,11 +132,11 @@ def detect_diameter(namefile_txt, num_values):
         D_shell = np.NaN      
 
     if not np.isnan(D_core) and not np.isnan(D_shell) and len(diameter_shell1)>len(diameter_core1) :
-        if ((len(diameter_core1)/len(diameter_shell1))<(0.3) ) or ((D_shell - D_core) < 0.05*num_values):
+        if ((len(diameter_core1)/len(diameter_shell1))<(0.4) ) or ((D_shell - D_core) < 0.05*num_values):
             D_core = np.NaN
             
     if not np.isnan(D_core) and not np.isnan(D_shell) and len(diameter_shell1)<len(diameter_core1) :
-        if ((len(diameter_shell1)/len(diameter_core1))<(0.3) ) or ((D_shell - D_core) < 0.05*num_values):
+        if ((len(diameter_shell1)/len(diameter_core1))<(0.4) ) or ((D_shell - D_core) < 0.05*num_values):
             D_shell = np.NaN
  # ------------------------------# run detect.py in yolov5----------------------------------------
 st.title('YOLOv5 Streamlit App')
