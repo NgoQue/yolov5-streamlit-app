a55609646759483a8cdf597f9d6a76859f18a7e7
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-15 01:04:48 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ def detect_diameter(namefile_txt, num_values):
                 diameter_core1 = diameter_core
                 break
             # if diameter_core[i] < min(diameter_shell):
-            if diameter_core[i] < 1.2*mean_core:
+            if diameter_core[i] < 1.1*mean_core:
                 diameter_core1 = diameter_core1 + [diameter_core[i]]
         D_core = np.mean(diameter_core1)
     else:
