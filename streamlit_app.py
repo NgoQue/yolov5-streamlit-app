c736b6563a601f492b995708a6c3414689f0929c
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-15 00:07:19 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,8 +34,8 @@ if uploaded_file is not None:
         new_width = int((width / height) * 640)
         image = image.resize((new_width, 640))
 
-        brightness_factor = 0.7  # Điều chỉnh giá trị để thay đổi độ sáng
-        # brightness_factor = np.random.uniform(0.65, 0.8)
+        # brightness_factor = 0.7  # Điều chỉnh giá trị để thay đổi độ sáng
+        brightness_factor = np.random.uniform(0.65, 0.8)
         enhancer = ImageEnhance.Brightness(image)
         image = enhancer.enhance(brightness_factor)
         
