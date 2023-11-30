5b0a763e70e115afa47ee90720ae89906a32117d
NgoQue <110570104+NgoQue@users.noreply.github.com>
2023-11-30 13:50:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -329,13 +329,13 @@ if st.button("Run Detection"):
                 fig, ax = plt.subplots()
                 
                 column_0 = scattering_cross_sections[:, 0]
-                ax.plot( wavelengths,column_0,'b',  label='qext', marker='s', markersize=10, markevery=10)
+                ax.plot( wavelengths,column_0,'b',  label='qext', marker='s', markersize=3, markevery=10)
                 
                 column_1 = scattering_cross_sections[:, 1]
-                ax.plot( wavelengths,column_1,'r', label='qsca', marker='o', markersize=10, markevery=10)
+                ax.plot( wavelengths,column_1,'r', label='qsca', marker='o', markersize=4, markevery=10)
                 
                 column_2 = scattering_cross_sections[:, 2]
-                ax.plot( wavelengths,column_0,'g', label='qabs', marker='^', markersize=10, markevery=11)
+                ax.plot( wavelengths,column_0,'g', label='qabs', marker='^', markersize=5, markevery=11)
 
                 ax.set_xlim(200, 1000)
                 ax.set_ylim(np.amin(scattering_cross_sections[:, 0:3]), np.amax(scattering_cross_sections[:, 0:3]))
