9b4efb10f958d2e9986c339fc90daae682f8da67
NgoQue <110570104+NgoQue@users.noreply.github.com>
2024-02-06 10:42:15 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,10 +62,10 @@ material_shell = st.sidebar.selectbox(
     "Select material shell",
     ['Au', 'Ag', 'Al2O3','Cu', 'Co', 'Cr', 'Fe2O3', 'Ge', 'MgO', 'Ni', 'Pb', 'Pt','Si', 'SiO2', 'TiO2'], index=None)
 
-light_intensity = st.sidebar.number_input('Enter the value of light intensity I0 (W/cm2) into the box below.', value=1, step=0.1)
+light_intensity = st.sidebar.number_input('Enter the value of light intensity I0 (W/cm2) into the box below.', value=1.0, step=0.1)
 I0 = light_intensity * 1e4  
 
-N = st.sidebar.number_input('Enter the value the number of particles per volume (N) into the box below. Please note that the unit for N is N*1e18.', value=1, step=0.1)
+N = st.sidebar.number_input('Enter the value the number of particles per volume (N) into the box below. Please note that the unit for N is N*1e18.', value=1.0, step=0.1)
 N = N * 1e18  
 
 # st.sidebar.write('The current number is ', number)
